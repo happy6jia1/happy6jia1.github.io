dd8d2949afa88bbec6a446ae8df1a189afcfa375
happy6jia1 <18312177725@163.com>
2023-11-26 16:34:14 +0800

    Site updated: 2023-11-26 16:34:08

--- js/readPercent.js
+++ js/readPercent.js
@@ -6,7 +6,7 @@ function percent() {
         result = Math.round(a / b * 100), // 计算百分比
         up = document.querySelector("#go-up") // 获取按钮
 
-    if (result <= 95) {
+    if (result <= 98) {
         up.childNodes[0].style.display = 'none'
         up.childNodes[1].style.display = 'block'
         up.childNodes[1].innerHTML = result;
